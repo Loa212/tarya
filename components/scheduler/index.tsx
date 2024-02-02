40d79c24f09651336dc353cdd82038ee5a2ba88e
Loa212 <anobileloris@gmail.com>
2024-02-02 19:52:04 +0100

    selectable calendar

--- components/scheduler/index.tsx
+++ components/scheduler/index.tsx
@@ -1,14 +1,35 @@
 "use client";
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import dayGrid from "@fullcalendar/daygrid";
+import timegrid from "@fullcalendar/timegrid";
+import interactions from "@fullcalendar/interaction";
+import { format, subMinutes } from "date-fns";
 
 const Schedule = () => {
   return (
     <FullCalendar
-      height={640}
-      plugins={[dayGridPlugin]}
+      headerToolbar={{
+        left: "title",
+        center: "prev,next",
+        right: "today,dayGridMonth,timeGridWeek,timeGridDay",
+      }}
       initialView="dayGridMonth"
+      height={640}
+      plugins={[dayGrid, timegrid, interactions]}
+      editable={true}
+      selectable={true}
+      select={(e) => {
+        alert(
+          "selected " +
+            e.startStr +
+            " to " +
+            format(subMinutes(e.end, 1), "yyyy-MM-dd") +
+            " (all-day: " +
+            e.allDay +
+            ")",
+        );
+      }}
       events={[
         { title: "event 1", date: "2024-02-08" },
         { title: "event 2", date: "2024-02-09" },
